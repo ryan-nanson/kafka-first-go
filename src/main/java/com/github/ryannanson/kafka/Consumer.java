abcf35fbd9d74f27e4ce174e66e83d46513d79cc
Ryan Nanson <32265264+ryan-nanson@users.noreply.github.com>
2020-03-30 14:12:46 +0100

    subscribe consumer to topic

--- src/main/java/com/github/ryannanson/kafka/Consumer.java
+++ src/main/java/com/github/ryannanson/kafka/Consumer.java
@@ -1,14 +1,55 @@
 package com.github.ryannanson.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class Consumer {
 
+    // Logger.
     private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
 
-    public static void main(String[] args) {
-        LOGGER.info("Hello world");
+    // Constant address of kafka server.
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
 
+    // constant Group ID
+    private static final String GROUP_ID = "application";
+
+    public static void main(String[] args) {
+
+        // Create Consumer properties.
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create consumer.
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // Subscribe consumer to topic.
+        String topic = "first_topic";
+
+        // Use Arrays.asList to allow subscribe to multiple topics if required.
+        consumer.subscribe(Arrays.asList(topic));
+
+        // Poll for new data.
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord record : records) {
+                LOGGER.info("Key: " + record.key() + ", Value: " + record.value());
+                LOGGER.info("Partition: " + record.partition() + ", Offset: "  + record.offset());
+            }
+        }
     }
 }
