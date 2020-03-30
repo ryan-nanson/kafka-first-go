a75b4a80d8ab081fa5e4ba479628fba9183fe3aa
Ryan Nanson <32265264+ryan-nanson@users.noreply.github.com>
2020-03-30 12:52:12 +0100

    Create Consumer.java

---
+++ src/main/java/com/github/ryannanson/kafka/Consumer.java
@@ -0,0 +1,14 @@
+package com.github.ryannanson.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Consumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world");
+
+    }
+}
