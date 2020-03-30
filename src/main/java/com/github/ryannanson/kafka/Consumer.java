39d4d015af3c30b9f96a1dcf9de50ec907709d84
Ryan Nanson <32265264+ryan-nanson@users.noreply.github.com>
2020-03-30 17:12:45 +0100

    Add multi threading to consumer

--- src/main/java/com/github/ryannanson/kafka/Consumer.java
+++ src/main/java/com/github/ryannanson/kafka/Consumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,45 +12,113 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class Consumer {
-
-    // Logger.
-    private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
 
     // Constant address of kafka server.
     private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
 
-    // constant Group ID
-    private static final String GROUP_ID = "application";
+    // Constant Group ID
+    private static final String GROUP_ID = "new_application";
+
+    // Constant for topic.
+    private static final String TOPIC = "first_topic";
 
     public static void main(String[] args) {
 
-        // Create Consumer properties.
-        Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        new Consumer().run();
+    }
 
-        // Create consumer.
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+    public Consumer() {
 
-        // Subscribe consumer to topic.
-        String topic = "first_topic";
+    }
 
-        // Use Arrays.asList to allow subscribe to multiple topics if required.
-        consumer.subscribe(Arrays.asList(topic));
+    public void run() {
+        Logger logger = LoggerFactory.getLogger(Consumer.class);
 
-        // Poll for new data.
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+        // Latch for dealing with multiple threads.
+        CountDownLatch latch = new CountDownLatch(1);
 
-            for (ConsumerRecord record : records) {
-                LOGGER.info("Key: " + record.key() + ", Value: " + record.value());
-                LOGGER.info("Partition: " + record.partition() + ", Offset: "  + record.offset());
+        // Create the consumer runnable.
+        logger.info("Creating the consumer thread");
+        Runnable consumerRunnable = new ConsumerRunnable(latch);
+
+        // Start the thread.
+        Thread thread = new Thread(consumerRunnable);
+        thread.start();
+
+        // Add shutdown hook.
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdown hook.");
+            ((ConsumerRunnable) consumerRunnable).shutdown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            logger.info("Application has exited.");
+        }
+        ));
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application got interrupted", e);
+        } finally {
+            logger.info("Application is closing.");
+        }
+    }
+
+    public static class ConsumerRunnable implements Runnable {
+
+        private Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class);
+        private CountDownLatch latch;
+        private KafkaConsumer<String, String> consumer;
+
+        public ConsumerRunnable(CountDownLatch latch) {
+            this.latch = latch;
+
+            // Create Consumer properties.
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+            // Create consumer.
+            consumer = new KafkaConsumer<String, String>(properties);
+
+            // Use Arrays.asList to allow subscribe to multiple topics if required.
+            consumer.subscribe(Arrays.asList(TOPIC));
+        }
+
+        @Override
+        public void run() {
+            // Poll for new data.
+            try {
+                while (true) {
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                    for (ConsumerRecord record : records) {
+                        logger.info("Key: " + record.key() + ", Value: " + record.value());
+                        logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                    }
+                }
+            } catch (WakeupException e) {
+                logger.info("Received shutdown signal!");
+            } finally {
+                consumer.close();
+
+                // Tell our main code we're done with the consumer.
+                latch.countDown();
+            }
+        }
+
+        public void shutdown() {
+            // Special method to interrupt consumer.poll() by throwing WakeUpException.
+            consumer.wakeup();
         }
     }
 }
